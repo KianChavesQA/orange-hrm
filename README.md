643f19f7665a3edb8ca3b4258036ac789b2e7180
Kian Chaves <kianchaves@live.com>
2025-08-16 11:46:27 -0300

    first commit

--- README.md
+++ README.md
@@ -31,3 +31,55 @@ Este projeto é um estudo realizado pela equipe Lumestack, focado em automação
 ## Contato
 
 Kian Chaves Oliveira - Email: kianchaves@live.com
+
+import "chai";
+
+//Orange HRM Login Test
+describe("Orange HRM Tests", () => {
+
+// Before each it visist the Login Page
+
+beforeEach(() => {
+cy.visit(
+"https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+);
+});
+
+// Selectors List
+
+const selectorsList = {
+usernameField: "[name='username']",
+passwordField: "[name='password']",
+loginButton: '[type="submit"]',
+alertMessage: "[role='alert']",
+sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+dasboardGrid: ".orangehrm-dashboard-grid"
+};
+
+const userData = {
+validUsername: "Admin",
+validPassword: "admin123",
+invalidUsername: "errorUser",
+invalidPassword: "errorPass"
+};
+
+// Sucessful Login
+
+it("Login - Sucess", () => {
+cy.get(selectorsList.usernameField).type(userData.validUsername);
+cy.get(selectorsList.passwordField).type(userData.validPassword);
+cy.get(selectorsList.loginButton).click();
+cy.location("pathname").should("equal", "/web/index.php/dashboard/index");
+cy.get(selectorsList.sectionTitleTopBar).should("contain", "Dashboard");
+cy.get(selectorsList.dasboardGrid).should("be.visible");
+});
+
+// Uncessfull Login
+
+it("Login - Fail", () => {
+cy.get(selectorsList.usernameField).type(userData.invalidUsername);
+cy.get(selectorsList.passwordField).type(userData.invalidPassword);
+cy.get(selectorsList.loginButton).click();
+cy.get(selectorsList.alertMessage).should("contain", "Invalid");
+});
+});
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,8 +1,7 @@
 //Orange HRM Login Test
 describe("Orange HRM Tests", () => {
+  // Before each it visist the Login Page
 
-  // Before each it visist the login page
-  
   beforeEach(() => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
@@ -17,23 +16,32 @@ describe("Orange HRM Tests", () => {
     loginButton: '[type="submit"]',
     alertMessage: "[role='alert']",
     sectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    dasboardGrid: ".orangehrm-dashboard-grid",
+  };
+
+  const userData = {
+    validUsername: "Admin",
+    validPassword: "admin123",
+    invalidUsername: "errorUser",
+    invalidPassword: "errorPass",
   };
 
   // Sucessful  Login
 
   it("Login - Sucess", () => {
-    cy.get(selectorsList.usernameField).type("Admin");
-    cy.get(selectorsList.passwordField).type("admin123");
+    cy.get(selectorsList.usernameField).type(userData.validUsername);
+    cy.get(selectorsList.passwordField).type(userData.validPassword);
     cy.get(selectorsList.loginButton).click();
     cy.location("pathname").should("equal", "/web/index.php/dashboard/index");
     cy.get(selectorsList.sectionTitleTopBar).should("contain", "Dashboard");
+    cy.get(selectorsList.dasboardGrid).should("be.visible");
   });
 
   // Uncessfull Login
 
   it("Login - Fail", () => {
-    cy.get(selectorsList.usernameField).type("errorUser");
-    cy.get(selectorsList.passwordField).type("admin123");
+    cy.get(selectorsList.usernameField).type(userData.invalidUsername);
+    cy.get(selectorsList.passwordField).type(userData.invalidPassword);
     cy.get(selectorsList.loginButton).click();
     cy.get(selectorsList.alertMessage).should("contain", "Invalid");
   });
